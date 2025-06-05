7f38cf68e6e79564b5b69b2b79997d35d0336ab2
huzaifa311 <huzaifahabib311@gmail.com>
2025-06-06 04:38:10 +0500

    update

--- web-offer/index.php
+++ web-offer/index.php
@@ -26,7 +26,7 @@ include 'include/header.php'; ?>
             <div class="h-4 w-4 rounded-full flex items-center justify-center bg-white whitespace-nowrap">
               <img src="assets/images/check.png" alt="tick" class="h-[11px] object-fit">
             </div>
-            <p class="text-white text-[15px] font-light	">3 Revisions
+            <p class="text-white text-[15px] font-light	">5 Revisions
             </p>
           </div>
         </li>
